a55d9f21a263834c3a636ce7f099e7f6b0d724db
Brad Touesnard <brad@touesnard.com>
2011-11-25 12:51:14 -0400

    Bug fix.

--- view/part/product-item.php
+++ view/part/product-item.php
@@ -1,5 +1,7 @@
 <a href="<?php echo $this->product_url(); ?>" class="product product-<?php echo $product->id; ?>">
-    <img src="<?php echo $product->image->src; ?>" width="<?php echo $product->image->width; ?>" height="<?php echo $product->image->height; ?>" alt="<?php echo esc_attr( $product->title ); ?>" />
+    <div class="screenshot">
+        <img src="<?php echo $product->image->src; ?>" width="<?php echo $product->image->width; ?>" height="<?php echo $product->image->height; ?>" alt="<?php echo esc_attr( $product->title ); ?>" />
+    </div>
     <div class="caption">
         <h4><?php echo $product->title; ?></h4>
         <p class="publisher">by <?php echo $this->publisher_list(); ?></p>
