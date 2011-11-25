b1afc053f529fcd96480c24165f6b1593be944c4
Brad Touesnard <brad@touesnard.com>
2011-11-25 10:52:11 -0400

    Adding PHP 5 checking.

--- wp-app-store.php
+++ wp-app-store.php
@@ -9,6 +9,17 @@ Version: 1.0
 License: GPL v2 - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 */
 
+if ( version_compare( PHP_VERSION, '5.2', '<' ) ) {
+    // Thanks for this Yoast!
+	if ( is_admin() && ( !defined( 'DOING_AJAX' ) || !DOING_AJAX ) ) {
+		require_once ABSPATH.'/wp-admin/includes/plugin.php';
+		deactivate_plugins( __FILE__ );
+	    wp_die( __('WP App Store requires PHP 5.2 or higher, as does WordPress 3.2 and higher. The plugin has now disabled itself.', 'wp-app-store' ) );
+	} else {
+		return;
+	}
+}
+
 require 'lib/wp-app-store.php' ;
 
 global $wp_app_store;
