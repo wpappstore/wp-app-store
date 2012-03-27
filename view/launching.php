056b0d98ae1733a532350705bce41c95b38a44d4
Brad Touesnard <brad@touesnard.com>
2012-03-27 15:11:35 -0300

    Launching soon msg.

---
+++ view/launching.php
@@ -0,0 +1,9 @@
+<div style="margin: 200px; background-color: lightYellow; border: 1px solid #E6DB55; padding: 0.4em 2em; text-align: center;">
+    <h2>Launching April 2012</h2>
+    <p style="font-size: 14px; line-height: 1.4em;">
+        We are still preparing for launch. Sign up to our mailing list at
+        <a href="http://wpappstore.com/">wpappstore.com</a> to be notified when
+        we launch.
+    </p>
+</div>
+
